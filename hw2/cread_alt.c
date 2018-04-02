81b649c8c4130e1e8cefcf3d769d6ea35fdc3d00
terryyn <yeyunong@hotmail.com>
2018-04-01 22:05:47 -0700

    Add files via upload

---
+++ hw2/cread_alt.c
@@ -0,0 +1,28 @@
+long cread(long * xp)
+{
+  long zero=0;
+  return *(xp ? xp: &zero);
+}
+
+/*
+        .file   "cread_alt.c"
+        .text
+        .globl  cread
+        .type   cread, @function
+cread:
+.LFB0:
+        .cfi_startproc
+        movq    $0, -8(%rsp)
+        leaq    -8(%rsp), %rax
+        testq   %rdi, %rdi
+        cmove   %rax, %rdi
+        movq    (%rdi), %rax
+        ret
+        .cfi_endproc
+.LFE0:
+        .size   cread, .-cread
+        .ident  "GCC: (GNU) 4.8.5 20150623 (Red Hat 4.8.5-16)"
+        .section        .note.GNU-stack,"",@progbits
+*/
+
+
